1bbedb13d707a169059b30ff6331eeedd289628c
Justin Lietz <jlietz93@gmail.com>
2024-11-10 16:50:01 -0800

    created app

---
+++ Marathon/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using Marathon.Models;
+using Newtonsoft.Json;
+
+namespace Marathon;
+
+public partial class MainPage : ContentPage
+{
+    private RaceCollection RaceObject;
+    public MainPage()
+    {
+        InitializeComponent();
+        Title = "Marathon Manager";
+        FillPicker();
+    }
+
+    public void FillPicker()
+    {
+        var client = new HttpClient();
+        client.BaseAddress = new Uri("https://joewetzel.com/fvtc/marathon/");
+        var Response = client.GetAsync("races/").Result;
+        var wsJson = Response.Content.ReadAsStringAsync().Result;
+
+        RaceObject = JsonConvert.DeserializeObject<RaceCollection>(wsJson);
+
+        RacePicker.ItemsSource = RaceObject.races;
+    }
+
+    private void RacePicker_OnSelectedIndexChanged(object sender, EventArgs e)
+    {
+        var selectedRace = ((Picker)sender).SelectedIndex;
+        var raceID = RaceObject.races[selectedRace].id;
+
+        var client = new HttpClient();
+        client.BaseAddress = new Uri("https://joewetzel.com/fvtc/marathon/");
+        var Response = client.GetAsync("results/" + raceID).Result;
+        var wsJson = Response.Content.ReadAsStringAsync().Result;
+        
+        var ResultObject = JsonConvert.DeserializeObject<ResultCollection>(wsJson);
+
+        var CellTemplate = new DataTemplate(typeof(TextCell));
+        CellTemplate.SetBinding(TextCell.TextProperty, "name");
+        CellTemplate.SetBinding(TextCell.DetailProperty, "detail");
+
+
+        lstResults.ItemTemplate = CellTemplate;
+        lstResults.ItemsSource = ResultObject.results;
+    }
+}
